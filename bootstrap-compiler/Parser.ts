88d968a0cbe789bc2eb7c2e5ae9f5c963eef9971
qucumbah <39967396+qucumbah@users.noreply.github.com>
2022-05-07 16:05:41 +0300

    parser documentation

--- bootstrap-compiler/Parser.ts
+++ bootstrap-compiler/Parser.ts
@@ -7,10 +7,24 @@ import Type from './ast/Type.ts';
 import Statement from './ast/Statement.ts';
 import Expression from './ast/Expression.ts';
 
+/**
+ * Shorthand for `parseModule`
+ * 
+ * @param tokens iterator of tokens that compose this module.
+ *   It will be moved until all module tokens are consumed.
+ * @returns the resulting module
+ */
 export function parse(tokens: Iter<Token>): Module {
   return parseModule(tokens);
 }
 
+/**
+ * Currently, module is just a collection of functions, so we can parse them one-by-one
+ * 
+ * @param tokens iterator of tokens that compose this module.
+ *   It will be moved until all module tokens are consumed.
+ * @returns the resulting module
+ */
 function parseModule(tokens: Iter<Token>): Module {
   const funcs: Func[] = [];
   while (tokens.hasNext()) {
@@ -24,6 +38,13 @@ function parseModule(tokens: Iter<Token>): Module {
   };
 }
 
+/**
+ * Function consists of signature (identifier, return type, arguments) and body (statement array)
+ * 
+ * @param tokens iterator of tokens that compose this function.
+ *   It will be moved until all function tokens are consumed.
+ * @returns the resulting function
+ */
 function parseFunction(tokens: Iter<Token>): Func {
   expect(tokens.next(), 'func');
 
@@ -58,6 +79,14 @@ function parseFunction(tokens: Iter<Token>): Func {
   };
 }
 
+/**
+ * Argument consists of name (identifier) and type
+ * 
+ * @param tokens iterator of tokens that compose this argument.
+ *   It will be moved until all argument tokens (including the comma after the argument)
+ *   are consumed.
+ * @returns the resulting argument
+ */
 function parseArgument(tokens: Iter<Token>): Argument {
   expectType(tokens.peekNext(), 'identifier');
   const name: string = tokens.next().value;
@@ -65,6 +94,7 @@ function parseArgument(tokens: Iter<Token>): Argument {
   expect(tokens.next(), ':');
   const type: Type = expectType(tokens.next(), 'type') as Type;
 
+  // Consume trailing comma
   if (tokens.peekNext().value === ',') {
     tokens.next();
   }
@@ -75,6 +105,13 @@ function parseArgument(tokens: Iter<Token>): Argument {
   };
 }
 
+/**
+ * Function for parsing different kinds of statements: conditionals, loops, returns, etc.
+ * 
+ * @param tokens iterator of tokens that compose this statement.
+ *   It will be moved until all statement tokens (including `;`) are consumed.
+ * @returns the resulting argument
+ */
 function parseStatement(tokens: Iter<Token>): Statement {
   switch (tokens.peekNext().value) {
     case 'if': return parseConditionalStatement(tokens);
@@ -134,6 +171,7 @@ function parseVariableDeclarationStatement(tokens: Iter<Token>): Statement {
   expect(tokens.next(), ':');
   const variableType: Type = expectType(tokens.next(), 'type') as Type;
 
+  // For now, we have to initialize the newly declared variable. This may change.
   expect(tokens.next(), '=');
 
   const value: Expression = parseExpression(tokens);
@@ -171,6 +209,10 @@ function parseExpressionStatement(tokens: Iter<Token>): Statement {
   };
 }
 
+/**
+ * Operator precedence, from lowest to highest.
+ * This will be used to determine the order of operations when an expression is parsed.
+ */
 const operatorPrecenence: ReadonlyArray<ReadonlyArray<typeof BinaryOperators[number]>> = [
   ['<', '<=', '>', '>='],
   ['=='],
@@ -178,6 +220,13 @@ const operatorPrecenence: ReadonlyArray<ReadonlyArray<typeof BinaryOperators[num
   ['*', '/'],
 ];
 
+/**
+ * Expressions are parsed using recursive descent.
+ * 
+ * @param tokens iterator of tokens that compose this expression.
+ *   It will be moved until all expression tokens are consumed.
+ * @returns the resulting expression
+ */
 function parseExpression(tokens: Iter<Token>): Expression {
   const expressionParseResult: ExpressionParseResult = parseExpressionInner(tokens);
 
@@ -186,21 +235,55 @@ function parseExpression(tokens: Iter<Token>): Expression {
   }
 
   while (tokens.peekNext() !== expressionParseResult.tokensAfter.peekNext()) {
-    // Skip tokens from parsed expression
+    // `parseExpressionInner` clones the token iterator internally
+    // and doesn't modify the provided one.
+    // Thus, we have to manually skip tokens from parsed expression.
     tokens.next();
   }
 
   return expressionParseResult.expression;
 }
 
+/**
+ * Expression parsing result.
+ * 
+ * Recursive descent parsing is a trial and error technique
+ * thus we have to have an easy way to check whether a parsing portion was successful.
+ * 
+ * For the same reason, we don't want to modify the provided tokens iterator,
+ * so we clone it internally and return the cloned one if parsing was successful.
+ * It should be advanced a few tokens deep in this case.
+ * 
+ * In future, this may contain the invalid token if any errors are encountered.
+ */
 type ExpressionParseResult = {
   error: true,
+  // failingToken: Token,
 } | {
   error: false,
   expression: Expression,
   tokensAfter: Iter<Token>,
 };
 
+/**
+ * Parse the expression (or an expression portion) using recursive descent.
+ * 
+ * Parses whatever it can until it runs out of options.
+ * If whatever was parsed makes sense, returns it.
+ * If there were errors in all possible trees, returns an error.
+ * 
+ * @param tokens tokens iterator positioned at the start of the statement.
+ * It will not be modified due to the trial-and-error nature of the recursive descent algorithm.
+ * 
+ * @param level exprected operator precenence level.
+ * Default value is 0 (for the lowest operator precedence).
+ * This function will call itself recursively, increasing the level on each call.
+ * If we run out of levels for operator precedence (level == operator precedence dictionary length),
+ * there are no more binary operators left, and the remaining expressions are basic.
+ * 
+ * @returns parsing result, which contains error indicator, the resulting expression,
+ * and the advanced tokens iterator
+ */
 function parseExpressionInner(tokens: Iter<Token>, level = 0): ExpressionParseResult {
   // Make a clone in case we're passing an impossible expression.
   // This way we don't have to revert iterator state.
@@ -344,12 +427,26 @@ function parseExpressionInner(tokens: Iter<Token>, level = 0): ExpressionParseRe
   }
 }
 
+/**
+ * Assertion for token value
+ * 
+ * @param token provided token
+ * @param value expected token value
+ */
 function expect(token: Token, value: string): void {
   if (token.value !== value) {
     throw new Error(`Unexpected token: ${token.value}. Expected: ${value}`);
   }
 }
 
+/**
+ * Assertion for token type
+ * 
+ * @param token provided token
+ * @param type expected token tyoe
+ * 
+ * @returns provided token value (for convenience)
+ */
 function expectType(token: Token, type: typeof token.type): string {
   if (token.type !== type) {
     throw new Error(`Unexpected token type: ${token.type}. Expected: ${type}`);
