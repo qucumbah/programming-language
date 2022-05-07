251625b28767ff54917ec4b26b63f69b1ac85bff
qucumbah <39967396+qucumbah@users.noreply.github.com>
2022-05-07 18:55:58 +0300

    document lexer

--- bootstrap-compiler/Lexer.ts
+++ bootstrap-compiler/Lexer.ts
@@ -77,6 +77,12 @@ type TokenPosition = {
   colEnd: number,
 };
 
+/**
+ * Run lexer on the provided source code (or source code slice).
+ * 
+ * @param source the source code of the program
+ * @returns resulting array of tokens
+ */
 export function lex(source: string): Token[] {
   const lines: string[] = source.split('\n');
   const linesWithoutComments: string[] = lines.map((line: string) => removeComments(line));
@@ -85,6 +91,12 @@ export function lex(source: string): Token[] {
     .flat();
 }
 
+/**
+ * Removes comments by finding the first '//' occurrence and removing it and everything after it.
+ * 
+ * @param line the line to remove comments from
+ * @returns the line with removed comments
+ */
 function removeComments(line: string): string {
   if (!line.includes('//')) {
     return line;
@@ -93,10 +105,26 @@ function removeComments(line: string): string {
   return line.slice(0, line.indexOf('//')).trim();
 }
 
+/**
+ * Perform lexing on a single line.
+ * 
+ * @param line line to lex
+ * @param lineIndex line index (needed for token positions)
+ * @returns resulting array of tokens
+ */
 function lexLine(line: string, lineIndex: number): Token[] {
+  // We're only going to separate by these sequences
+  // Separating by anything else is redundant and invalid
+  // E.g. if we separate by keywords, we may accidentally separate an identifier
+  // that includes the keyword
   const separators: string[] = [...Whitespace, ...Operators, ...Specials];
   const result: Token[] = [];
 
+  // High-level overview:
+  // For each of the chars in the line, look if this char is the start of a separator.
+  // If it is, then everything behind it is either an identifier or a string of length 0
+  // Add a token for everything behind the separator, and another token for separator itself
+  // After that, cut the string after the separator (`start` is the start of the cut string)
   let start = 0;
   let cur = 0;
   while (cur < line.length) {
@@ -132,6 +160,15 @@ function lexLine(line: string, lineIndex: number): Token[] {
   return result;
 }
 
+/**
+ * Creates a token with content and position.
+ * 
+ * @param line line to create the token from
+ * @param lineIndex line number where the token is positioned at (0-based)
+ * @param start start position of the token on the line (inclusive)
+ * @param end end position of the token on the line (exclusive)
+ * @returns a token with type, value and position
+ */
 function createToken(line: string, lineIndex: number, start: number, end: number): Token {
   const tokenValue: string = line.slice(start, end);
 
