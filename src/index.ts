e4e62e88f106cb20119dd2853149874b6a84ec71
qucumbah <39967396+qucumbah@users.noreply.github.com>
2022-05-16 15:59:48 +0300

    clean up command line interface

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,10 @@
+import { CompilerOptions, parseCliArguments } from "./CliArgumentsParser.ts";
 import { compile } from "./lang/Compiler.ts";
-import { Token } from "./lang/lexer/Token.ts";
 
 async function main(args: string[]) {
-  const source: string = await Deno.readTextFile('./examples/generation-test.lctwa');
+  const [mainFile, _options]: [string, CompilerOptions] = parseCliArguments(args);
+  const source: string = await Deno.readTextFile(mainFile);
   const result: string = compile(source);
-  console.log(result);
-}
-
-function printProgramOutOfTokens(tokens: Token[]) {
-  let result = '';
-  let prevLine = 1;
-  for (const token of tokens) {
-    if (token.line !== prevLine) {
-      result += '\n';
-      prevLine = token.line;
-    }
-
-    result += token.value;
-    result += ' ';
-  }
   console.log(result);
 }
 
