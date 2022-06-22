45f48bb09aa4c73004c1a103c4f82329f9a5b08b
qucumbah <39967396+qucumbah@users.noreply.github.com>
2022-06-22 20:24:03 +0300

    start work on example framework

---
+++ examples/app/main.js
@@ -0,0 +1,17 @@
+async function main() {
+  const wasm = await getWasm();
+  wasm.instance.exports.init(100, 100);
+
+  step(wasm);
+}
+
+function step(wasm) {
+  wasm.instance.exports.step();
+  requestAnimationFrame(() => step(wasm));
+}
+
+async function getWasm() {
+  return WebAssembly.instantiate(await fetch('main.wasm'));
+}
+
+main();
