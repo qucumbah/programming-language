bb88cb72e3fa0c6499456bf612b00c77511642a6
qucumbah <39967396+qucumbah@users.noreply.github.com>
2022-07-16 20:29:18 +0300

    make preview app actually compile input

--- preview-app/app.ts
+++ preview-app/app.ts
@@ -1,6 +1,41 @@
 import { compile } from "../src/lang/Compiler.ts";
 
-import webassemblyjs from "webassemblyjs";
+async function main() {
+  const binaryen: any = await getBinaryen();
+  const source = document.querySelector("#source") as HTMLTextAreaElement;
+  const output = document.querySelector("#output") as HTMLTextAreaElement;
+  const compileButton = document.querySelector(
+    "#compileButton"
+  ) as HTMLButtonElement;
+  compileButton.addEventListener("click", async () => {
+    const result = await getModuleFromSource(binaryen, source.value);
+    output.value = result.toString();
+  });
+}
 
-const module = webassemblyjs.instantiateFromSource(compile(""));
-console.log(module);
+async function getModuleFromSource(
+  binaryen: any,
+  source: string
+): Promise<WebAssembly.Instance> {
+  const compilationResult: string = compile(source);
+  const internalModule = binaryen.parseText(compilationResult);
+  const module = new WebAssembly.Module(internalModule.emitBinary());
+  const instance: WebAssembly.Instance = await WebAssembly.instantiate(module);
+  console.log(instance);
+  return instance;
+}
+
+/**
+ * Although browsers understand ES modules quite well, Deno doesn't like them as much.
+ * Thus, we can't use a direct import, so we have to import via eval.
+ * Eval is needed because bundler is too smart and tries to bundle the ES module from the CDN,
+ * which is exactly what causes Deno to fail.
+ */
+async function getBinaryen() {
+  const { default: binaryen } = await (0, eval)(
+    "import('https://esm.sh/binaryen@109.0.0')"
+  );
+  return binaryen;
+}
+
+main();
